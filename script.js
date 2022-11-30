9a927c6e310698b97f99e9459abc264454597570
ymgcst <shota.yamaguchi@hopin.co.jp>
2022-11-30 21:23:24 +0900

    いろいろ機能追加

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let g_isPlayer1 = true
+
 document.addEventListener('DOMContentLoaded', function (){
 
   // DOM読み込み完了時に呼ばれるハンドラ
 
+  DrawBlock()
+
 });
 
 function OnClick(e, isPlayer1, point) {
+  if (g_isPlayer1 != isPlayer1) {
+    return
+  }
+
   // 現在のマークの次のマークに更新
   const text = e.target.innerText
   if (text == "") {
@@ -34,10 +42,10 @@ function OnClick(e, isPlayer1, point) {
     // 加点処理
     let element
     if(isPlayer1) {
-      element = document.getElementById("player1")
+      element = document.getElementById("player1-point")
     }
     else {
-      element = document.getElementById("player2")
+      element = document.getElementById("player2-point")
     }
     let num = Number(element.innerText)
     num += point
@@ -48,12 +56,38 @@ function OnClick(e, isPlayer1, point) {
   }
 }
 
+function Next() {
+  g_isPlayer1 = !g_isPlayer1
+
+  if (g_isPlayer1) {
+    NextRound()
+  }
+
+  DrawBlock()
+}
+
 function NextRound() {
   let element = document.getElementById("round")
 
   // "Round 1"の数字だけを抽出
-  let numRound = Number(element.innerText.slice(-2))
-  numRound++
+  const numRound = Number(element.innerText.slice(-2)) + 1
 
-  element.innerText = "Round " + numRound
+  element.innerText = `Round ${numRound}`
+}
+
+function DrawBlock() {
+  let p1Blocks = Array.from(document.querySelectorAll(".player1-block"))
+  let p2Blocks = Array.from(document.querySelectorAll(".player2-block"))
+
+  let p1Color = g_isPlayer1 ? "red" : "aliceblue"
+  let p2Color = g_isPlayer1 ? "aliceblue" : "blue"
+
+  for (const el of p1Blocks) {
+    el.style.borderColor = p1Color
+  }
+  for (const el of p2Blocks) {
+    el.style.borderColor = p2Color
+  }
+  document.getElementById("player1-point").style.borderColor = p1Color
+  document.getElementById("player2-point").style.borderColor = p2Color
 }
